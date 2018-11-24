0335afd14670b44e19f88e395c573c961deef510
Andrew Grant <andy.a.grant@gmail.com>
2018-11-24 10:42:20 +0000

    Defined some domain types

---
+++ src/Program.fs
@@ -0,0 +1,9 @@
+﻿module DomainTypes =
+    type Candidate = Candidate of string
+    type Ballot = Candidate list
+
+module Program =
+    [<EntryPoint>]
+    let main argv =
+        printfn "Hello World from F#!"
+        0
